14557ba9e19e63a34ebceccdd552c16cbfc0190d
khai-dv <khai-dv@fujinet.vn>
2017-10-13 17:25:18 +0700

    Khai-DV code 2017/10/13

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -16,8 +16,9 @@ export class UserService {
 	// Put Edit Item : http://58e605d279739c1200ed3c82.mockapi.io/api/users/:id
 	// Delete Item : http://58e605d279739c1200ed3c82.mockapi.io/api/users/:id
 	//private apiUrl = 'http://59d448b95803340011fd5f25.mockapi.io/users/'
-	private apiUrl = 'http://59d464da5803340011fd5f49.mockapi.io/V1/Users'
-	
+	//private apiUrl = 'http://59d464da5803340011fd5f49.mockapi.io/V1/Users'
+	private apiUrl = "http://59d464185803340011fd5f44.mockapi.io/Users"
+
 	constructor(
 		private _httpService : Http
 		
