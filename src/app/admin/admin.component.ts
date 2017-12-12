86594a7041f919b0570082981c5c43e9a5ea3277
Phạm Thanh Luân <luan-pt@FUJINET.vn>
2017-12-12 14:58:30 +0700

    Add - Admin  Page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -13,38 +13,157 @@ import * as EXL from 'ts-xlsx';
 
 export class AdminComponent implements OnInit {  
 
+  private  Url= "../assets/data/IProduct.xlsx";
   errorMessage: string;
   products: IProduct[] ;
-  newproduct: IProduct;
+  IMproducts: any;
+  public isDisabled: boolean;
 
-  constructor(private _productService: ProductService,
+  constructor(public _productService: ProductService,
               public mygb : AppGlobals) {
                 
     this.mygb.shareObj['namepage']='admin';
   }
 
-  public ImportProducts():void{
+  public ImportProducts(pdService:ProductService){
+    this.isDisabled = false;
+    this.ReadExcel(this._productService);
+    alert(this.isDisabled);
+    //this.isDisabled = fasle;
+    alert(this.isDisabled);
     
-    this.DeleteAll();
-    let wb: EXL.IWorkBook = EXL.read("../assets/data/IProduct.xlsx")
-    this._productService.addItem(this.newproduct);
   }
 
+  public ReadExcel(pdService:ProductService){
+  
+    this.DeleteAll();
+
+    this.makeFileRequest(function(result){
+      //do whatever you want now with the output data
+      //console.log(result);
+
+      result.forEach(element => {
+        //console.log(element.product_id);
+        pdService.addItem(element).subscribe(
+            //Do something
+            );
+      });
+    });
+  }
   
   DeleteAll():void{
     this._productService.getItems()
     .subscribe(products => {
       // set items to json response
-      this.products = products;
+      products.map(element => {
+        //alert(element.product_id);
+        this._productService.deleteItem("id",element.product_id)
+        .subscribe(
+          result => console.log(result),
+          error => this.errorMessage = error
+        );  
+      });
+  
     },
       error => this.errorMessage = <any>error);
+  }
+
+  makeFileRequest(callback){
+
+    var oReq = new XMLHttpRequest();
+
+    oReq.open("GET", this.Url, true);
+    oReq.responseType = "arraybuffer";
+
+    function asyncReq(callback){ 
+      oReq.onload = function(e) {
+        var arraybuffer = oReq.response;
+      
+        /* convert data to binary string */
+        var data = new Uint8Array(arraybuffer);
+        var arr = new Array();
+        for(var i = 0; i != data.length; ++i){ 
+          arr[i] = String.fromCharCode(data[i]);
+          //  console.log("Data"+data[i]);
+        }
+        var bstr = arr.join("");
+        var workbook = EXL.read(bstr, {type:"binary"});
+        //console.log("Data"+bstr);
+        var first_sheet_name = workbook.SheetNames[0];
+          /* Get worksheet */
+        var worksheet = workbook.Sheets[first_sheet_name]; 
+        var json = EXL.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header:0, raw:true});
+        var jsonOut = JSON.stringify(json);
+        
+        // return getProduct(json);
+        //console.log("test"+json);
+        callback(json);
+      }
+    }
     
-    this.products.forEach(element => {
-      this._productService.deleteItem(element.product_id);
+    oReq.send();
+    asyncReq(function(result){
+      //do whatever you want now with the output data
+      //console.log(IMpd);
+      callback(result);
     });
   }
 
+    // makeFileRequest(url: string, params: Array<any>) {
+    //   return new Promise((resolve, reject) => {
+
+    //     function getProduct(json:any, IMproducts:Array<any>){
+          
+    //       json.forEach(element=>{ 
+    //         //alert(element.product_id);
+    //         IMproducts.push(
+    //         {
+    //           "product_id" : element.product_id,
+    //           "product_code" : element.product_code,
+    //           "product_name" : element.product_name,
+    //           "price" : element.price,
+    //           "product_type" : element.product_type,
+    //           "imageUrl" : element.imageUrl,
+    //           "product_detail" : element.product_detail
+    //         });
+    //       });
+    //       alert("getProduct" + IMproducts.length);
+    //     }
+
+    //     var formData: any = new FormData();
+    //     var xhr = new XMLHttpRequest();
   
+    //     xhr.open("GET", url, true);
+    //     xhr.responseType = 'arraybuffer';
+  
+    //     xhr.onload = function () {
+    //       if (xhr.readyState === xhr.DONE) {
+    //         var arraybuffer = xhr.response;
+            
+    //           /* convert data to binary string */
+    //           var data = new Uint8Array(arraybuffer);
+    //           var arr = new Array();
+    //           for(var i = 0; i != data.length; ++i){ 
+    //             arr[i] = String.fromCharCode(data[i]);
+    //             //  console.log("Data"+data[i]);
+    //           }
+    //           var bstr = arr.join("");
+    //           var workbook = EXL.read(bstr, {type:"binary"});
+    //           //console.log("Data"+bstr);
+    //           var first_sheet_name = workbook.SheetNames[0];
+    //             /* Get worksheet */
+    //           var worksheet = workbook.Sheets[first_sheet_name]; 
+    //           var json = EXL.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header:0, raw:true});
+    //           var jsonOut = JSON.stringify(json);
+    //           getProduct(json,params);
+    //           debugger; 
+    //           alert("params" + params.length);
+    //       }
+    //     };
+    //     xhr.send(formData);
+    //   });
+    // }
+
   ngOnInit(): void {
 
   }
