ba17e8943ef411f769026951ed043ec0ceabd787
proculy <proculy@yahoo.com>
2017-12-13 10:00:08 +0700

    complete productDetails

--- src/app/Product-details/product-details.component.ts
+++ src/app/Product-details/product-details.component.ts
@@ -17,6 +17,8 @@ import { Subscription } from 'rxjs/Subscription';
 import { IProduct } from '../defines/product.interface';
 import { ProductService } from '../services/product.service';
 import { AppGlobals } from '../app.globals';
+import { ActivatedRouteSnapshot } from '@angular/router/src/router_state';
+import { Observable } from "rxjs/Rx";
 
 @Component({
     selector: 'app-product-details',
@@ -25,12 +27,9 @@ import { AppGlobals } from '../app.globals';
 })
 export class ProductDetailsComponent implements OnInit {
     pageTitle: string = 'Product Detail';
-    public product: any={};
+    public product: any = {};
     public productID: number;
-    // product: any = {};
-    // imageUrl: string;
     errorMessage: string;
-    // private sub: Subscription;
 
     constructor(private route: ActivatedRoute,
         private router: Router,
@@ -39,44 +38,20 @@ export class ProductDetailsComponent implements OnInit {
         public mygb: AppGlobals) {
 
         this.mygb.shareObj['namepage'] = 'productDetails';
-
-        this.ac_route.queryParams.subscribe(params => {
-            this.productID = parseInt( params["itemsearch"]);
-            // alert("AAA" + this.productID);
-            this.getProduct(this.productID);
-            });
     }
+
     ngOnInit(): void {
-        
-        // this.sub = this.route.params.subscribe(params => {
-        //     console.log(this.productID);
-        //     this.getProduct(this.productID);
-        // });
-
-        this.getProduct(this.productID);
+        this.route.params.subscribe(params => {
+            this.productID = +params['id'];
+            this.getProduct(this.productID);
+        });
     }
-
-
-    // ngOnDestroy() {
-    //     this.sub.unsubscribe();
-    // }
-
-
-
+    
     getProduct(id: number) {
         this.productService.getItem(id).subscribe(
             product => {
-                console.log(product);
                 this.product = product
             },
             error => this.errorMessage = <any>error);
     }
-
-    // onBack(): void {
-    //     this.router.navigate(['/products']);
-    // }
-
-    // onRatingClicked(message: string): void {
-    //     this.pageTitle = 'Product Detail: ' + message;
-    // }
 }
