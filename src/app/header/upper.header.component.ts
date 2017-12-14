50cd5d3df9bb04692e811068f8bdd6d3f8de43c0
Ngô Thị Hồng Sen <sen-nth@fujinet.vn>
2017-12-14 15:56:19 +0700

    add gio hanh tu list, favoris

--- src/app/header/upper.header.component.ts
+++ src/app/header/upper.header.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ICart } from '../defines/cart.interface';
+import { CartshopService } from '../services/cartshop.service';
+import { AppGlobals } from '../app.globals';
 
 @Component({
   selector: 'app-upper-header',
-  templateUrl: './upper.header.component.html'
+  templateUrl: './upper.header.component.html',
+  providers: [CartshopService]
 })
 
-export class UpperComponent {
-  totalCart: number=10;
-  totalCurrency: number=442.6667;
+export class UpperComponent implements OnInit {
+
+  errorMessage: string;
+  pro_carts: ICart[];
+  totalCart: number;
+  totalCurrency:number=0;
+
+  constructor(private _cartService: CartshopService,
+    public mygb: AppGlobals) {
+    this.mygb.shareObj['namepage'] = 'upper_header';
+  }
+
+  ngOnInit(): void {
+    this._cartService.getItems()
+      .subscribe(pro_carts => {
+        this.pro_carts = pro_carts;
+        this.totalCart = pro_carts.length;
+        this.totalCurrency = 0;
+        for(let i of pro_carts){
+          this.totalCurrency = this.totalCurrency + i.total;
+        }        
+      },
+      error => this.errorMessage = <any>error);
+
+  }
 }
