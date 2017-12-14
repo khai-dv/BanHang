937d19eef79fe962789ddffe14b662f4c7ec3dba
Trần Văn Nhực <nhuc-tv@fujinet.vn>
2017-12-14 14:18:17 +0700

    Xoa tootip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ import { LoginComponent } from './login/index';
 import { CartshopComponent } from './cartshop/index';
 import { AdminComponent } from './admin/index';
 import { ProductFavoriteComponent } from './product-favorite/index';
-import { TooltipDirective } from 'ng2-tooltip-directive/components';
 
 let providers = {
     "google": {
@@ -65,8 +64,7 @@ let providers = {
     GeneralContentComponent,
     CartshopComponent,
     AdminComponent,
-    ProductFavoriteComponent,
-    TooltipDirective
+    ProductFavoriteComponent
   ],
   imports: [
     BrowserModule,
